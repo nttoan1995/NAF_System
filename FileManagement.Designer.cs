7e681bb2a19e934c44ce12f5ad4a5bfd2ee6ffac
nttoan1995 <thanhtoan12th@gmail.com>
2016-05-31 22:47:26 +0700

    Add files via upload

---
+++ FileManagement.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace DA5
+{
+    partial class filemanagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encryptFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decryptFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToUseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listfoder = new System.Windows.Forms.ListBox();
+            this.listsubfoder = new System.Windows.Forms.ListBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.opemToolStripMenuItem,
+            this.newFolderToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.moveToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // opemToolStripMenuItem
+            // 
+            this.opemToolStripMenuItem.Name = "opemToolStripMenuItem";
+            this.opemToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.opemToolStripMenuItem.Text = "Open";
+            this.opemToolStripMenuItem.Click += new System.EventHandler(this.opemToolStripMenuItem_Click);
+            // 
+            // newFolderToolStripMenuItem
+            // 
+            this.newFolderToolStripMenuItem.Name = "newFolderToolStripMenuItem";
+            this.newFolderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newFolderToolStripMenuItem.Text = "New folder";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // moveToolStripMenuItem
+            // 
+            this.moveToolStripMenuItem.Name = "moveToolStripMenuItem";
+            this.moveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.moveToolStripMenuItem.Text = "Move";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolToolStripMenuItem
+            // 
+            this.toolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportFileToolStripMenuItem,
+            this.encryptFileToolStripMenuItem,
+            this.decryptFileToolStripMenuItem});
+            this.toolToolStripMenuItem.Name = "toolToolStripMenuItem";
+            this.toolToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.toolToolStripMenuItem.Text = "Tool";
+            // 
+            // exportFileToolStripMenuItem
+            // 
+            this.exportFileToolStripMenuItem.Name = "exportFileToolStripMenuItem";
+            this.exportFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportFileToolStripMenuItem.Text = "Export file";
+            // 
+            // encryptFileToolStripMenuItem
+            // 
+            this.encryptFileToolStripMenuItem.Name = "encryptFileToolStripMenuItem";
+            this.encryptFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.encryptFileToolStripMenuItem.Text = "Encrypt file";
+            // 
+            // decryptFileToolStripMenuItem
+            // 
+            this.decryptFileToolStripMenuItem.Name = "decryptFileToolStripMenuItem";
+            this.decryptFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.decryptFileToolStripMenuItem.Text = "Decrypt file";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToUseToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // howToUseToolStripMenuItem
+            // 
+            this.howToUseToolStripMenuItem.Name = "howToUseToolStripMenuItem";
+            this.howToUseToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.howToUseToolStripMenuItem.Text = "How to use";
+            // 
+            // listfoder
+            // 
+            this.listfoder.FormattingEnabled = true;
+            this.listfoder.Location = new System.Drawing.Point(6, 55);
+            this.listfoder.Name = "listfoder";
+            this.listfoder.Size = new System.Drawing.Size(132, 160);
+            this.listfoder.TabIndex = 1;
+            this.listfoder.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listfoder_MouseDoubleClick);
+            // 
+            // listsubfoder
+            // 
+            this.listsubfoder.FormattingEnabled = true;
+            this.listsubfoder.Location = new System.Drawing.Point(144, 55);
+            this.listsubfoder.Name = "listsubfoder";
+            this.listsubfoder.Size = new System.Drawing.Size(134, 160);
+            this.listsubfoder.TabIndex = 2;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 29);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(140, 20);
+            this.output.TabIndex = 3;
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // filemanagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.listsubfoder);
+            this.Controls.Add(this.listfoder);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "filemanagement";
+            this.Text = "File Management";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem encryptFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decryptFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToUseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opemToolStripMenuItem;
+        private System.Windows.Forms.ListBox listfoder;
+        private System.Windows.Forms.ListBox listsubfoder;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.ToolStripMenuItem newFolderToolStripMenuItem;
+    }
+}
+
---
+++ FileManagement.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DA5
+{
+    public partial class filemanagement : Form
+    {
+        public string patch = "";
+        public filemanagement()
+        {
+            InitializeComponent();
+        }
+
+        private void opemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            patch = "";
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter = "NA file| *.na";
+            if (ofile.ShowDialog() == DialogResult.OK)
+            {
+                patch = output.Text = ofile.FileName;
+            }
+            if (patch != "")
+            {
+                getfolder(patch, 64, listfoder);
+            }
+        }
+        /// <summary>
+        /// Lấy thông tin file/folder
+        /// </summary>
+        /// <param name="patch">Đường dẫn tập tin NA</param>
+        /// <param name="pos">Vị trí bắt đầu của vùng mô tả thư mục</param>
+        /// <param name="l">ListBox muốn thêm nội dung vào</param>
+
+        private void getfolder(string patch, int pos, ListBox l)
+        {
+            l.Items.Clear();
+            string fileformat = "", name = "";
+            int count = 0, countread = 32; ;
+            byte[] entry = new byte[32];
+            FileStream fs = new FileStream(patch, FileMode.Open, FileAccess.Read);
+            fs.Seek(pos, SeekOrigin.Begin);
+            fs.Read(entry, 0, 32);
+            // Dò tìm các entry trên thư mục hiện tại
+            while(entry[0] == '%' && entry[1] != 0 && entry[2] != 0 && countread < 512)
+            {
+                name = "";
+                fileformat = "";
+                // Đọc phần định dạng file từ byte 0 -> byte 4 của entry hiện tại
+                for (int i = 1; i < 5; i++)
+                {
+                    byte t = entry[i];
+                    if (t != 0x00)
+                        fileformat += (char)t;
+                }
+                // Đọc phần tên của tập tin
+                for (int i = 5; i < 21; i++)
+                {
+                    byte t = entry[i];
+                    if (t != 0x00)
+                        name += (char)t;
+                }
+                count++;
+                name = count.ToString() + ". " + name;
+                if (fileformat != "SYS")
+                    name = name + "." + fileformat;
+                l.Items.Add(name);
+                fs.Read(entry, 0, 32);
+                countread += 32;
+            }
+            if (count == 0)
+                l.Items.Add("Can not find folder or file");
+            fs.Close(); 
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// Tạo mới file nA
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            byte[] valnull = new byte[1024];
+            byte[] sign = new byte[64];
+            sign[0] = (byte)'%';
+            sign[1] = (byte)'N';
+            sign[2] = (byte)'A';
+            sign[3] = (byte)'F';
+            //---------------------
+            for (int i = 4; i < 64; i++)
+                sign[i] = 0;
+            for (int i = 0; i < 1024; i++)
+                valnull[i] = 0;
+            //---------------------
+            //Chọn đường dẫn lưu tập tin
+            SaveFileDialog saveFileDlg = new SaveFileDialog();
+            saveFileDlg.Filter = "NA file| *.na";
+            if (saveFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                patch = output.Text = saveFileDlg.FileName;
+            }
+            //Tạo tập tin khi có đường dẫn
+            if (patch != "")
+            {
+                if (File.Exists(patch))
+                    File.Delete(patch);
+                FileStream fs = new FileStream(patch, FileMode.Append, FileAccess.Write);
+                //Thêm phần dành riêng
+                fs.Write(sign, 0, 64);
+                //Thêm vào phần mô tả hệ thống thư mục (18 Sector)
+                for (int i = 0; i < 9; i++)
+                    fs.Write(valnull, 0, valnull.Length);
+                    //Thêm vào bảng FAT
+                for (int i = 0; i < 12288; i++)
+                    fs.Write(valnull, 0, 1024);
+                fs.Close();
+            }
+        }
+
+        private void listfoder_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string t = listfoder.GetItemText(listfoder.SelectedItem);
+            MessageBox.Show(t);
+        }
+
+        
+    }
+}
